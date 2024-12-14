8a67756a55cec03ca687fe6d592066ccae0da1b1
ARMIE JOY TANGALIN <s.ajutangalin@usm.edu.ph>
2024-12-14 22:16:45 +0800

    Website - Final Project
    Website - Final Project

--- IntroToComputing-Website-Project/script/script_final.js
+++ IntroToComputing-Website-Project/script/script_final.js
@@ -151,3 +151,10 @@ function scrollToBloomTracker() {
 function scrollToSection(id) {
     document.getElementById(id).scrollIntoView({ behavior: 'smooth' });
 }
+
+document.querySelector('a[href^="#"]').addEventListener('click', function(e) {
+  e.preventDefault();
+  document.querySelector(this.getAttribute('href')).scrollIntoView({
+    behavior: 'smooth'
+  });
+});
