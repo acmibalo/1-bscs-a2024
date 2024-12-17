448a8c9e2c051920c455deff2d0047ae623dd99a
Rachel Joy P. Pacot <s.rjppacot@usm.edu.ph>
2024-12-18 00:36:53 +0800

    Chapter 12: Add Ons

---
+++ Challenge #12/main.js
@@ -0,0 +1,72 @@
+// Responsive Navigation
+function toggleNav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+
+// Modal Box
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+// Tabs
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+// Progress Bar
+function move() {
+    var elem = document.getElementById("myProgress");
+    var width = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (width >= 100) {
+            clearInterval(id);
+        } else {
+            width++;
+            elem.value = width;
+        }
+    }
+}
+
+// Image Lightbox
+var modalImg = document.getElementById("myModalImg");
+var img = document.getElementById("myImg");
+var modalImgContent = document.getElementById("img01");
+
+img.onclick = function(){
+    modalImg.style.display = "block";
+    modalImgContent.src = this.src;
+}
+
+modalImg.onclick = function() { 
+    modalImg.style.display = "none";
+}
